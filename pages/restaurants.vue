68dcc89661d87ca555d75329f7113e0a5858307f
phanuell-365 <phanuelkidakwa@outlook.com>
2022-07-25 17:02:39 +0300

    Added the restaurants.vue route/page into the pages directory

---
+++ pages/restaurants.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>
+      <NavBar/>
+      <div class="container text-center mt-5">
+        <h1>These are the available restaurants</h1>
+        <a href="/restaurants" class="link-success">Go to restaurants</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "restaurants"
+}
+</script>
+
+<style scoped>
+
+</style>
