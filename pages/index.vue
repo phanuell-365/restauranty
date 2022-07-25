1a873511c1dcd4d488cc6856c93523d97650e8b1
phanuell-365 <phanuelkidakwa@outlook.com>
2022-07-25 16:56:10 +0300

    Added the pages directory and deleted the app.vue file

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <NavBar/>
+    <div class="container text-center mt-5">
+      <h1>Welcome to Restaurantly</h1>
+      <a href="/restaurants" class="link-success">Go to restaurants</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
